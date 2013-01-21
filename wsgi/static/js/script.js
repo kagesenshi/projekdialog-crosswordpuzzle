b9f4c267452d8ffa2b9a2ede7906ca8c4a7ea715
Izhar Firdaus <izhar@inigo-tech.com>
2013-01-21 11:15:14 +0800

    query remote json

--- wsgi/static/js/script.js
+++ wsgi/static/js/script.js
@@ -3,7 +3,8 @@
 		// provide crossword entries in an array of objects like the following example
 		// Position refers to the numerical order of an entry. Each position can have 
 		// two entries: an across entry and a down entry
-        $.get('puzzle.json', function (data) {	
+        var jsonurl = $('#puzzle-wrapper').attr('data-crossword-url');
+        $.get(jsonurl, function (data) {	
     		$('#puzzle-wrapper').crossword(data);
         }, 'json');
 		
